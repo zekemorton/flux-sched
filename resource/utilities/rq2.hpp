fedf2ec2f2996b1c99d4cb747d98f35a2ef39f46
Daniel Milroy <daniel.milroy@gmail.com>
2022-08-03 14:58:10 -0700

    rq2: adding skeleton initialization of resource-query replacement

---
+++ resource/utilities/rq2.hpp
@@ -0,0 +1,19 @@
+/*****************************************************************************\
+ * Copyright 2022 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, LICENSE)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\*****************************************************************************/
+
+#ifndef RQ2_HPP
+#define RQ2_HPP
+
+#include <memory>
+#include <cerrno>
+#include <vector>
+#include <map>
+#include "resource/hlapi/bindings/c++/reapi_cli.hpp"
+#include "resource/hlapi/bindings/c++/reapi_cli_impl.hpp"
