1b95eb5ced90e88b9ffb6f70fd0bf5b3bca81199
Daniel Milroy <daniel.milroy@gmail.com>
2022-08-03 14:57:24 -0700

    rq: create prototype rq2 using reapi
    Problem: Resource API duplicates much of the functionality of
    resource query. Most of the ci testing framework is built on top
    of resource query, and thus does not test the resource api. Any
    new functionality added must also be added to both rq and reapi
    
    Create a new resource query that uses resource api.

---
+++ resource/utilities/rq2.hpp
@@ -0,0 +1,26 @@
+/*****************************************************************************\
+ * Copyright 2022 Lawrence Livermore National Security, LLC
+ * (c.f. AUTHORS, NOTICE.LLNS, LICENSE)
+ *
+ * This file is part of the Flux resource manager framework.
+ * For details, see https://github.com/flux-framework.
+ *
+ * SPDX-License-Identifier: LGPL-3.0
+\*****************************************************************************/
+
+#include <memory>
+#include <cerrno>
+#include <vector>
+#include <map>
+#include "resource/reapi/bindings/c++/reapi_cli.hpp"
+#include "resource/reapi/bindings/c++/reapi_cli_impl.hpp"
+
+using namespace Flux;
+using namespace Flux::resource_model;
+using namespace Flux::resource_model::detail;
+
+int match (resource_query_t &ctx, std::vector<std::string> &args);
+int info (resource_query_t &ctx, std::vector<std::string> &args);
+int help (resource_query_t &ctx, std::vector<std::string> &args);
+int quit (resource_query_t &ctx, std::vector<std::string> &args);
+int cancel (resource_query_t &ctx, std::vector<std::string> &args);
