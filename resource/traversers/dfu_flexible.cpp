db75786309c23010de532b5ae08708a6e590ebe0
Zeke Morton <morton30@llnl.gov>
2026-04-14 13:52:26 -0700

    flexible traverser: add support for xor slots
    Problem: xor slots require multiple calls to select for
    each of the different xor slot options.
    
    Overload select to split the job spec resources and loop
    through xor slot options.

--- resource/traversers/dfu_flexible.cpp
+++ resource/traversers/dfu_flexible.cpp
@@ -165,7 +165,7 @@ void dfu_flexible_t::prime_jobspec (std::vector<Resource> &resources,
 
         // Or slots should use a minimum of values rather than an accumulation
         // otherwise possible matches may be filtered out
-        if (resource.type == slot_rt) {
+        if (resource.type == slot_rt or resource.type == xor_slot_rt) {
             for (const auto &aggregate : resource.user_data) {
                 min_if (subsystem,
                         aggregate.first,
@@ -189,6 +189,32 @@ void dfu_flexible_t::prime_jobspec (std::vector<Resource> &resources,
             }
         }
     }
+}
+
+int dfu_flexible_t::select (Jobspec::Jobspec &j, vtx_t root, jobmeta_t &meta, bool excl)
+{
+    auto xor_resources = split_xor_slots (j.resources);
+
+    if (xor_resources.empty ()) {
+        m_err_msg += __FUNCTION__;
+        m_err_msg += ": split_xor_slots failed.\n";
+        if (errno != 0) {
+            m_err_msg += strerror (errno);
+            m_err_msg += "\n";
+        }
+        return -1;
+    }
+
+    for (auto &resources : xor_resources) {
+        if (select_resources (root, resources, meta, excl) == 0) {
+            // only set the jobspec with the new
+            // resources if there's a match
+            j.resources = resources;
+            return 0;
+        }
+    }
+
+    return -1;
 }
 
 std::vector<std::vector<Resource>> dfu_flexible_t::split_xor_slots (
